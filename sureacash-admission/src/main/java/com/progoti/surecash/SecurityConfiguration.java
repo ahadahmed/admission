e2335c8b6df2a16b967bdf8662a9b326e3372693
ahad <ahad.ahmed@surecash.net>
2017-09-27 16:17:29 +0600

    Disable csrf for spring security.

--- sureacash-admission/src/main/java/com/progoti/surecash/SecurityConfiguration.java
+++ sureacash-admission/src/main/java/com/progoti/surecash/SecurityConfiguration.java
@@ -38,11 +38,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 //		super.configure(http);
 		http
 			.authorizeRequests()
-			.antMatchers("/","/resources/**","/home").permitAll()
+			.antMatchers("/","/resources/**","/home","/academic/validate-info").permitAll()
 			.antMatchers("/login").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
+			.csrf().disable()
 			.formLogin()
 			.loginPage("/login")
 			.failureUrl("/login?error=true")
