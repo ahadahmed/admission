37ba888b93e002ae6fd0e9aa73beaadf62d29217
ahad <ahad.ahmed@surecash.net>
2017-10-02 12:25:09 +0600

    Replace static user login by dynamic value from BD.

--- sureacash-admission/src/main/java/com/progoti/surecash/admission/service/UserLoginService.java
+++ sureacash-admission/src/main/java/com/progoti/surecash/admission/service/UserLoginService.java
@@ -1,27 +1,36 @@
 package com.progoti.surecash.admission.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.progoti.surecash.admission.domain.StudentInfo;
 import com.progoti.surecash.admission.domain.User;
 import com.progoti.surecash.admission.domain.UserDetailsImpl;
+import com.progoti.surecash.admission.repository.StudentInfoRepository;
 
 @Service
 public class UserLoginService implements UserDetailsService{
+	@Autowired
+	StudentInfoRepository studentInfoRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 		
+		StudentInfo student = studentInfoRepository.findOneByUserName(userName);
+		
 		User user = new User();
-		user.setUserName("testUser");
-		user.setPassword("123456");
+		user.setUserName(student.getUserName());
+		user.setPassword(student.getPassword());
 		user.setRole("USER");
-		user.setEmail("test@mail.com");
+		user.setEmail(student.getEmail());
 		
 		return new UserDetailsImpl(user);
 	}
+	
+	
 	
 	
 
