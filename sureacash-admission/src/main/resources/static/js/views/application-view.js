09b4845758118fa02c0d1467f68e637eb538944f
Emdadul Hoque <job.sawon90@gmail.com>
2017-10-05 11:04:26 +0600

    add action to delete button

--- sureacash-admission/src/main/resources/static/js/views/application-view.js
+++ sureacash-admission/src/main/resources/static/js/views/application-view.js
@@ -1,6 +1,21 @@
 
 function deleteUnit(elNum) {
     if (confirm("Are you sure?")) {
+        var row = $("#tbl-apl-row-" + elNum);
+        var unit = row.children('td:nth-child(1)').html();
+        var name = row.children('td:nth-child(2)').html();
+        var fees = row.children('td:nth-child(3)').html()
+        row.remove();
 
+        $("#tbl-available").find("tbody")
+        .append($("<tr>")
+                .append($("<td>")
+                    .append($("<input>").attr("type", "checkbox"))
+                    .append($("<span>").text(" " + unit)))
+                .append($("<td>").text(name))
+                .append($("<td>").text(fees))
+        );
+
+        //TODO: Call service with ID = elNum
     }
 }
