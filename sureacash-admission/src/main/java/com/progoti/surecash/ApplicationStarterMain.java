33257860a917696eeaa8b80a3c880403d88087f8
shaown <anjum@surecash.net>
2017-10-15 18:16:32 +0600

    change in main class for tomcat deployment

--- sureacash-admission/src/main/java/com/progoti/surecash/ApplicationStarterMain.java
+++ sureacash-admission/src/main/java/com/progoti/surecash/ApplicationStarterMain.java
@@ -2,13 +2,19 @@ package com.progoti.surecash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.progoti.surecash")
-public class ApplicationStarterMain {
+public class ApplicationStarterMain extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApplicationStarterMain.class);
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(ApplicationStarterMain.class, args);
 
 	}
